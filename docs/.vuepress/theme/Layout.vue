d257468810b3080db44f580ce9e3870522334eb9
myumo <794267701@qq.com>
2022-01-04 18:00:30 +0800

    first commit

---
+++ docs/.vuepress/theme/Layout.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="my-component">
+    <DemoComponent/>
+  </div>
+</template>
+
+<script>
+import DemoComponent from '../components/DemoComponent'
+
+export default {
+  name: 'MyComponent',
+  components: {
+    DemoComponent
+  },
+  mixins: [],
+  props: {},
+  data() {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created() {
+  },
+  mounted() {
+  },
+  destroyed() {
+  },
+  methods: {},
+}
+</script>
+
+<style lang="scss" scoped>
+#my-component {
+}
+</style>
